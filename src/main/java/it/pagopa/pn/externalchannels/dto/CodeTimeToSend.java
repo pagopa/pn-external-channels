6c03ead454ce8ab6027c90022c8ca71e0db99d45
vincenzo-racca-pa <vincenzo.racca@pagopa.it>
2022-10-10 16:20:55 +0200

    aggiunti log

--- src/main/java/it/pagopa/pn/externalchannels/dto/CodeTimeToSend.java
+++ src/main/java/it/pagopa/pn/externalchannels/dto/CodeTimeToSend.java
@@ -4,12 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.time.Duration;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class CodeTimeToSend {
+public class CodeTimeToSend implements Serializable {
 
     private String code;
     private Duration time;
