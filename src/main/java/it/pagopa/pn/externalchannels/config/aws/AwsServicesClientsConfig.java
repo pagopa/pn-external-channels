a620179e903faa004b0ce04962e8b9e15ac21b17
EmanueleBonofiglio <emanuele.bonofiglio@grupposcai.it>
2023-02-24 09:44:11 +0100

    PN-4158 fix pr

--- src/main/java/it/pagopa/pn/externalchannels/config/aws/AwsServicesClientsConfig.java
+++ src/main/java/it/pagopa/pn/externalchannels/config/aws/AwsServicesClientsConfig.java
@@ -1,53 +1,20 @@
 package it.pagopa.pn.externalchannels.config.aws;
 
 
+import it.pagopa.pn.commons.configs.RuntimeMode;
+import it.pagopa.pn.commons.configs.aws.AwsConfigs;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
-import software.amazon.awssdk.awscore.client.builder.AwsClientBuilder;
-import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.kms.KmsAsyncClient;
 
-import java.net.URI;
 
 @Configuration
 @Slf4j
-public class AwsServicesClientsConfig {
+public class AwsServicesClientsConfig extends it.pagopa.pn.commons.configs.aws.AwsServicesClientsConfig {
 
     private AwsConfigs props;
 
     public AwsServicesClientsConfig(AwsConfigs props) {
-        this.props=props;
-    }
-
-    @Bean
-    public KmsAsyncClient kmsAsyncClient() {
-        return configureBuilder(KmsAsyncClient.builder() );
-    }
-
-    private <C> C configureBuilder(AwsClientBuilder<?, C> builder) {
-        if( props != null ) {
-
-            String profileName = props.getProfileName();
-            if( StringUtils.isNotBlank( profileName ) ) {
-                builder.credentialsProvider( ProfileCredentialsProvider.create( profileName ));
-            }
-
-            String regionCode = props.getRegionCode();
-            if( StringUtils.isNotBlank( regionCode )) {
-                builder.region( Region.of( regionCode ));
-            }
-
-            String endpointUrl = props.getEndpointUrl();
-            if( StringUtils.isNotBlank( endpointUrl )) {
-                builder.endpointOverride( URI.create( endpointUrl ));
-            }
-
-        }
-
-        return builder.build();
+        super(props, RuntimeMode.PROD);
     }
 
 }
