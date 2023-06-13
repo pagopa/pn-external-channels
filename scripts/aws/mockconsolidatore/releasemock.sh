b4acf61843fb8fa3c1443fca66bb98e5195ac5a2
Federico Fatica <federico.fatica@pagopa.it>
2023-06-13 09:59:23 +0200

    [mock deploy] fix storage

--- scripts/aws/mockconsolidatore/releasemock.sh
+++ scripts/aws/mockconsolidatore/releasemock.sh
@@ -21,6 +21,7 @@ _BUCKETNAME=$3
 _ENV=$4
 _BUCKETPATH="mockconsolidatore"
 _STACK="mockconsolidatore"
+_STACK_INFRA="mockconsolidatore-storage"
 _WORKDIR="$PWD/target"
 _PARAMSFILE="$_WORKDIR/$_STACK-params.yaml"
 
@@ -30,15 +31,43 @@ _AWSCLI_CMD_PREFIX="--profile $_AWS_USERPROFILE  --region $_AWS_REGION"
 rm -rf $_WORKDIR
 mkdir -p $_WORKDIR
 
+cp $PWD/../cfn/storage.yml $_WORKDIR
+
+#### DEPLOY STACK STORAGE
+echo "=== CF Deploy Storage"
+aws $_AWSCLI_CMD_PREFIX \
+    cloudformation deploy \
+    --stack-name $_STACK_INFRA \
+    --template-file "$_WORKDIR/storage.yml" \
+    --capabilities CAPABILITY_NAMED_IAM \
+    --parameter-overrides ProjectName=pn \
+    MicroserviceNumber=0 \
+    TemplateBucketBaseUrl=- \
+    Version=-
+
+echo "== Read Outputs from Storage Stack $_STACK_INFRA"
+_STACK_INFRA_OUTPUTS=$(aws $_AWSCLI_CMD_PREFIX \
+    cloudformation describe-stacks \
+      --stack-name $_STACK_INFRA \
+      --query "Stacks[0].Outputs" \
+      --output json \
+      | jq -r '.[]  | "\"\(.OutputKey)=\(.OutputValue )\""' ) 
+
+
 ##### COPY STACK
 sed s/\!Sub\ \"\$\{TemplateBucketBaseUrl\}/\"./g $PWD/../cfn/microservice.yml > $_WORKDIR/$_STACK.yaml 
 cp -r $PWD/fragments $_WORKDIR
 
-
 ##### BUILD PARAMS
 echo "[" > $_PARAMSFILE.tmp
+echo $_STACK_INFRA_OUTPUTS | tr -s '" "' '","' >> $_PARAMSFILE.tmp
+if [ ! -z "${_STACK_INFRA_OUTPUTS}" ];
+then 
+  echo "," >> $_PARAMSFILE.tmp
+fi
 jq -r 'to_entries|map("\"\(.key)=\(.value)\"")|.[]' mockconsolidatore-$_ENV.json | tr '\n' ',' >> $_PARAMSFILE.tmp
 sed '$ s/.$/\]/' $_PARAMSFILE.tmp > $_PARAMSFILE
+
 
 ##### STACK PARAMS
 echo "=== CF Package"
