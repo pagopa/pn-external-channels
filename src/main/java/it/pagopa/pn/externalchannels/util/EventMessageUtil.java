490b050c50d1738f1bb0223c2cd88ab80b670af0
Giacomo Vallorani <giacomo.vallorani@pagopa.it>
2025-06-27 11:23:48 +0200

    fix(PN-15364): fix null pointer

--- src/main/java/it/pagopa/pn/externalchannels/util/EventMessageUtil.java
+++ src/main/java/it/pagopa/pn/externalchannels/util/EventMessageUtil.java
@@ -156,8 +156,9 @@ public class EventMessageUtil {
         String code = codeTimeToSend.getCode();
         boolean endsWithABCDEF = code.matches(".*[A-F]$");
         boolean isARChannel = AR.equals(notificationProgress.getChannel());
-        boolean isAutoDatetimeDisabled = codeTimeToSend.getAdditionalActions().stream()
-                .anyMatch(c -> c.getAction().equals(AdditionalAction.ADDITIONAL_ACTIONS.NO_AUTO_DATETIME));
+        boolean isAutoDatetimeDisabled = codeTimeToSend.getAdditionalActions() != null &&
+                codeTimeToSend.getAdditionalActions().stream()
+                    .anyMatch(c -> c.getAction().equals(AdditionalAction.ADDITIONAL_ACTIONS.NO_AUTO_DATETIME));
 
         if (endsWithABCDEF && isARChannel && !isAutoDatetimeDisabled) {
             log.info("Setting businessStatusDatetime for event. Code: {}, Channel: {}, Previous datetime: {}",
